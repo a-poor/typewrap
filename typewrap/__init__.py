0341153879886f2e6ec84cc82fe5fe5c8931df70
a-poor <45295232+a-poor@users.noreply.github.com>
2020-09-26 01:58:37 -0400

    Error wasn't successfully raising

--- typewrap/__init__.py
+++ typewrap/__init__.py
@@ -1,6 +1,6 @@
 
 import functools
-from inspect import signature, _empty
+import inspect
 
 
 __version__ = "0.0.1"
@@ -9,23 +9,23 @@ __version__ = "0.0.1"
 def typeCheck(f):
     """Function type-checking decorator."""
     # Get the function signature
-    sig = signature(f)
+    sig = inspect.signature(f)
     @functools.wraps(f)
     def inner(*args,**kwargs):
         # Check the inputs
         bound = sig.bind(*args,**kwargs)
         for arg, val in bound.arguments.items():
-             argtype = sig.parameters.get(arg).annotation
-             if argtype is _empty: continue
-             if not isinstance(val,argtype):
-                TypeError(f"typeCheck: type({val!r}) !+ {argtype!r}")
+            argtype = sig.parameters.get(arg).annotation
+            if argtype is inspect._empty: continue
+            if not isinstance(val,argtype):
+                raise TypeError(f"[typeCheck] type({val!r}) !+ {argtype!r}")
         # Compute the result
         result = f(*args,**kwargs)
         # Check the output
         return_type = sig.return_annotation
-        if return_type is not _empty:
+        if return_type is not inspect._empty:
             if not isinstance(result,return_type):
-                raise TypeError(f"typeCheck: type({result!r}) !+ {return_type!r}")
+                raise TypeError(f"[typeCheck] type({result!r}) !+ {return_type!r}")
         return result
     return inner
 
