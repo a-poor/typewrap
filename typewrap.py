1928ca1e9e94343561ec5a6d3fcbb436c6c36532
a-poor <45295232+a-poor@users.noreply.github.com>
2020-09-26 01:03:40 -0400

    Adding working wrapper

--- typewrap.py
+++ typewrap.py
@@ -1,38 +1,31 @@
 
 import functools
-from inspect import signature
+from inspect import signature, _empty
 
 
 def typeCheck(f):
-
-    annots = f.__annotations__
-
-    annots = {
-        k: v for k, v in 
-        f.__annotations__.items()
-        if isinstance(v, type)
-    }
-    inTypes = {
-        k:v for k,v in annots.items()
-        if k != "return"
-    }
-    checkOutput = "return" in annots
-    outType = annots.get("return")
-    
+    # Get the function signature
+    sig = signature(f)
     @functools.wraps(f)
     def inner(*args,**kwargs):
         # Check the inputs
-        # ...
+        bound = sig.bind(*args,**kwargs)
+        for arg, val in bound.arguments.items():
+             argtype = sig.parameters.get(arg).annotation
+             if argtype is _empty: continue
+             if not isinstance(val,argtype):
+                TypeError(f"typeCheck: type({val!r}) !+ {argtype!r}")
+
         # Compute the result
         result = f(*args,**kwargs)
         # Check the output
-        if output is not None:
-            assert (
-                isinstance(result,outType) or 
-                result == outType
-                ), f"return value typeCheck error: {result!r} != {outType!r}"
+        return_type = sig.return_annotation
+        if return_type is not _empty:
+            if not isinstance(result,return_type):
+                raise TypeError(f"typeCheck: type({result!r}) !+ {return_type!r}")
         return result
     return inner
+
 
 
 def add1(a,b):
