503ad0aba93253e9ff44c1fa2c5c04e4122f5e05
a-poor <45295232+a-poor@users.noreply.github.com>
2020-11-09 12:10:19 -0500

    Bump version: 0.2.3 → 0.2.4

--- typewrap/__init__.py
+++ typewrap/__init__.py
@@ -12,7 +12,7 @@ from typing import Callable, Any
 from .exceptions import TypeCheckError
 
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 
 
 def checkInputs(f: Callable[[Any],Any]) -> Callable[[Any],Any]:
